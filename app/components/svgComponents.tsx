210a31b22784a7fb43a471905f6e3378f475ca5c
Julian <julianmartinez_93@hotmail.com>
2023-02-04 16:07:38 -0300

    improve the seo desktop and movil

--- app/components/svgComponents.tsx
+++ app/components/svgComponents.tsx
@@ -1,6 +1,4 @@
 
-import * as React from 'react';
-
 export const CheckSvg = () => (
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 19.07 18'>
     <defs>
--- app/components/workComponent.tsx
+++ app/components/workComponent.tsx
@@ -1,4 +1,4 @@
-
+import { Suspense } from 'react';
 import Image from 'next/image';
 import { CheckSvg, HeaderSvg } from './';
 import { landingWork } from '../model';
@@ -10,12 +10,16 @@ export default function WorkComponent (prop: landingWork) {
       <HeaderSvg title={title} />
       <article className='flex relative max-w-full items-center'>
         <div className='flex flex-col gap-2 bottom-10 right-2 font-bold text-xs relative'>
-          <Image
-            className='w-auto h-auto  relative -translate-y-9 bit:translate-x-2 '
-            alt='background'
-            src={image}
-            priority
-          />
+
+          <Suspense fallback={<div>Loading...</div>}>
+            <Image
+              className='w-auto h-auto  relative -translate-y-9 bit:translate-x-2 '
+              alt='background'
+              src={image}
+              priority
+            />
+          </Suspense>
+
           <div className='flex pl-5 flex-col gap-2 relative -top-4 '>
             <p className='inline-flex items-center '><i className='inline-block w-4 mr-2'><CheckSvg /></i>{check1}</p>
             <p className='inline-flex items-center '><i className='inline-block w-4 mr-2'><CheckSvg /></i>{check2}</p>
@@ -25,6 +29,7 @@ export default function WorkComponent (prop: landingWork) {
         <p className=' flex-1 w-auto h-auto font-normal bit:text-base text-sm text-[#4B4B4B] text-center p-1  -translate-y-7'>
           {description}
         </p>
+
       </article>
     </>
   );
